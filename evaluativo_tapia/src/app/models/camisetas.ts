1bf036a40629771af2c5a361836dfcd97eb252ad
facutapiaa <110854459+facutapiaa@users.noreply.github.com>
2024-06-29 16:32:12 -0300

    Delete evaluativo_tapia directory

--- evaluativo_tapia/src/app/models/camisetas.ts
+++
@@ -1,8 +0,0 @@
-export interface Camisetas {
-    id: any; //atributos del tipo any recien vacios o indefinidos
-    club: string;
-    año:number;
-    talle:string;
-    img:string;
-    alt:string;
-}
